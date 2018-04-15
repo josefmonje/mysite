630e19fd84c5e1427be03bb2a72421d1d1b79ade
josefmonje <josefmonje@gmail.com>
2018-04-15 11:39:21 +0800

    symlink from pwd

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -s configs/supervisor.conf /etc/supervisor/conf.d/
+sudo ln -s $PWD/configs/supervisor.conf /etc/supervisor/conf.d/
 
-sudo ln -s configs/nginx.conf /etc/nginx/sites-available/
+sudo ln -s $PWD/configs/nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
