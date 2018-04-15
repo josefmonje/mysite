21e5552fb671c1fbc254fa5734e7ac038685bd7e
josefmonje <josefmonje@gmail.com>
2018-04-15 22:20:10 +0800

    full path symlinks

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo ln -s configs/supervisor.conf /etc/supervisor/conf.d/
+sudo ln -s ${HOME}/configs/supervisor.conf /etc/supervisor/conf.d/
 
-sudo ln -s configs/nginx.conf /etc/nginx/sites-available/
+sudo ln -s ${HOME}/configs/nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp /etc/nginx/sites-enabled/mysite.conf /etc/nginx/sites-enabled/default
