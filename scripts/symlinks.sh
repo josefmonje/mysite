e919c29cab37abaf88f1f80b76719a87d8990db1
josefmonje <josefmonje@gmail.com>
2018-04-15 19:34:08 +0800

    nospace

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -2,6 +2,5 @@
 
 PWD=${HOME}/mysite
 sudo ln -s $PWD/configs/supervisor.conf /etc/supervisor/conf.d/
-
 sudo ln -s $PWD/configs/nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
