ed3adcdbfe2068eb0d7507b1bb5cb4730eaf6bbf
josefmonje <josefmonje@gmail.com>
2018-04-15 20:39:08 +0800

    make nginx default

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -2,5 +2,8 @@
 
 PWD=${HOME}/mysite
 sudo ln -s $PWD/configs/supervisor.conf /etc/supervisor/conf.d/
+
 sudo ln -s $PWD/configs/nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp /etc/nginx/sites-enabled/mysite.conf /etc/nginx/sites-enabled/default
