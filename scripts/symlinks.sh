768ebc0b00e8c1baa0c6bdae2b45b0bb372eb9cb
josefmonje <josefmonje@gmail.com>
2018-04-15 22:34:09 +0800

    fix nginx symlinks

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -3,6 +3,6 @@
 sudo ln -s ${HOME}/configs/supervisor.conf /etc/supervisor/conf.d/
 
 sudo ln -s ${HOME}/configs/nginx.conf /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
+
 sudo rm /etc/nginx/sites-enabled/default
-sudo cp /etc/nginx/sites-enabled/mysite.conf /etc/nginx/sites-enabled/default
