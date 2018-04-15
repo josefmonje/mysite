a5129205298e37921602cf70d08800cecbcc86db
josefmonje <josefmonje@gmail.com>
2018-04-15 21:43:28 +0800

    full pipenv path

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -2,7 +2,7 @@
 
 sudo supervisorctl stop mysite
 
-pipenv run ./manage.py migrate
-pipenv run ./manage.py collectstatic --noinput
+/home/ubuntu/.local/bin/pipenv run ./manage.py migrate
+/home/ubuntu/.local/bin/pipenv run ./manage.py collectstatic --noinput
 
 sudo supervisorctl start mysite
