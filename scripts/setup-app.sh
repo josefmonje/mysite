3cc084fc65382dd9f443f07b4ac82b9096a0e4ab
josefmonje <josefmonje@gmail.com>
2018-04-15 20:54:10 +0800

    relative manage.py since pwd

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -3,7 +3,7 @@
 sudo supervisorctl stop mysite
 
 PWD=${HOME}/mysite
-/home/ubuntu/.local/bin/pipenv run ${PWD}/manage.py migrate
-/home/ubuntu/.local/bin/pipenv run ${PWD}/manage.py collectstatic --noinput
+/home/ubuntu/.local/bin/pipenv run ./manage.py migrate
+/home/ubuntu/.local/bin/pipenv run ./manage.py collectstatic --noinput
 
 sudo supervisorctl start mysite
