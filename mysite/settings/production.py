3e97306f7e4d0f9fbf293b35bf7091b680f65eae
josefmonje <josefmonje@gmail.com>
2018-04-15 20:54:33 +0800

    secret key env or default

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,11 @@
 from .base import *
 
+import os
+
 DEBUG = False
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.production")
+SECRET_KEY = os.environ.setdefault("SECRET_KEY", "am9zZWZtb25qZW15c2l0ZQo=")
 
 try:
     from .local import *
