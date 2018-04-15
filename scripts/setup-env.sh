71ce1c4c2f4d6ebf4b58ff01f86f137d27d0bd07
josefmonje <josefmonje@gmail.com>
2018-04-15 19:15:02 +0800

    include pipenv install in setup env

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -4,6 +4,8 @@ PWD=${HOME}/mysite
 ${PWD}/scripts/cronjobs.sh
 ${PWD}/scripts/symlinks.sh
 
+pipenv install --three
+
 sudo service nginx restart
 sudo service supervisor restart
 sudo service jenkins restart
