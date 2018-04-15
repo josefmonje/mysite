873e5fadfbf0465ee82c157db6425d0d581cedab
josefmonje <josefmonje@gmail.com>
2018-04-15 19:26:06 +0800

    space

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -2,5 +2,6 @@
 
 PWD=${HOME}/mysite
 sudo ln -s $PWD/configs/supervisor.conf /etc/supervisor/conf.d/
+
 sudo ln -s $PWD/configs/nginx.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
