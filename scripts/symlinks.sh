cf4a41ece14ca7c431bd7bc0d8aab054cd467f0e
josefmonje <josefmonje@gmail.com>
2018-04-15 11:03:53 +0800

    dont rename conf in symlink

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -s configs/supervisor.conf /etc/supervisor/conf.d/mysite.conf
+sudo ln -s configs/supervisor.conf /etc/supervisor/conf.d/
 
-sudo ln -s configs/nginx.conf /etc/nginx/sites-available/mysite.conf
-sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/mysite.conf
+sudo ln -s configs/nginx.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/mysite.conf /etc/nginx/sites-enabled/
