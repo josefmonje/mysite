46a335c77b42157a8f91a3b5082ce2ba74c75d4c
josefmonje <josefmonje@gmail.com>
2018-04-17 11:45:19 +0800

    set production settings in .env when launching from script

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -2,6 +2,8 @@
 
 sudo supervisorctl stop mysite
 
+echo "DJANGO_SETTINGS_MODULE=mysite.settings.production" > .env
+
 /home/ubuntu/.local/bin/pipenv run ./manage.py migrate
 /home/ubuntu/.local/bin/pipenv run ./manage.py collectstatic --noinput
 
