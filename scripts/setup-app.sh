2298cbbcbc5c027c00ded122bd8f8ed2e19b2d07
josefmonje <josefmonje@gmail.com>
2018-04-17 12:13:24 +0800

    set secret key in env

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -3,6 +3,7 @@
 sudo supervisorctl stop mysite
 
 echo "DJANGO_SETTINGS_MODULE=mysite.settings.production" > .env
+echo "SECRET_KEY=`date +%s | sha256sum | base64 | head -c 64`" >> .env
 
 /home/ubuntu/.local/bin/pipenv run ./manage.py migrate
 /home/ubuntu/.local/bin/pipenv run ./manage.py collectstatic --noinput
