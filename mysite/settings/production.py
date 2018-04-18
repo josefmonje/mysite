c266cdc46e35bc749b6cb7e770f0cb13026c8a31
josefmonje <josefmonje@gmail.com>
2018-04-18 10:45:55 +0800

    error 500 notification setting

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -2,6 +2,19 @@ from .base import *
 
 import os
 
+ADMINS = [
+    ('Josef', 'josefmonje@gmail.com'),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = None
+EMAIL_HOST_PASSWORD = None
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
