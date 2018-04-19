04d0e9039894170caa56179ad76b0e2643c17a19
josefmonje <josefmonje@gmail.com>
2018-04-19 10:37:00 +0800

    move collectstatus before stop mysite, rm env vars

--- scripts/setup-app.sh
+++ scripts/setup-app.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-sudo supervisorctl stop mysite
-
-echo "DJANGO_SETTINGS_MODULE=mysite.settings.production" > .env
-echo "SECRET_KEY=`date +%s | sha256sum | base64 | head -c 64`" >> .env
-
-/home/ubuntu/.local/bin/pipenv run ./manage.py migrate
 /home/ubuntu/.local/bin/pipenv run ./manage.py collectstatic --noinput
 
+sudo supervisorctl stop mysite
+
+/home/ubuntu/.local/bin/pipenv run ./manage.py migrate
+
 sudo supervisorctl start mysite
-sudo service nginx restart
