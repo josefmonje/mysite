cd90dede81ee2fa754fdd46cbe88bc8511136689
josefmonje <josefmonje@gmail.com>
2018-04-19 12:04:38 +0800

    load vars, echo for each alarm

--- scripts/setup-alarm.sh
+++ scripts/setup-alarm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Loading"
+source scripts/vars.sh
+
 echo "Creating SNS topic"
 alarmName1=status-mon
 alarmName2=disk-mon
@@ -14,9 +17,12 @@ aws sns subscribe --topic-arn $snsArn1 --protocol email --notification-endpoint 
 aws sns subscribe --topic-arn $snsArn2 --protocol email --notification-endpoint josefmonje@gmail.com
 echo "Done. Please check your email!"
 
-echo "Enabling Metric Alarms..."
+echo "Enabling Status Alarms"
 aws cloudwatch put-metric-alarm --alarm-name status-mon-1 --alarm-description "Alarm when Prod 1 isn't running" --metric-name StatusCheckFailed --namespace AWS/EC2 --statistic Maximum --period 60 --threshold 1 --comparison-operator GreaterThanOrEqualToThreshold --dimensions "Name=InstanceId,Value=$instanceId2" --evaluation-periods 1 --alarm-actions $snsArn1 --unit Count --treat-missing-data breaching
 aws cloudwatch put-metric-alarm --alarm-name status-mon-2 --alarm-description "Alarm when Prod 2 isn't running" --metric-name StatusCheckFailed --namespace AWS/EC2 --statistic Maximum --period 60 --threshold 1 --comparison-operator GreaterThanOrEqualToThreshold --dimensions "Name=InstanceId,Value=$instanceId3" --evaluation-periods 1 --alarm-actions $snsArn1 --unit Count --treat-missing-data breaching
+
+echo "Enabling Disk Alarms"
 aws cloudwatch put-metric-alarm --alarm-name disk-mon-1 --alarm-description "Alarm when Prod 1 storage exceeds 80%" --metric-name DiskSpaceUtilization --namespace System/Linux --statistic Maximum --period 60 --threshold 80 --comparison-operator GreaterThanThreshold --evaluation-periods 1 --alarm-actions $snsArn2 --unit Percent --dimensions Name=InstanceId,Value=$instanceId2 Name=MountPath,Value=/ Name=Filesystem,Value=/dev/xvda1
 aws cloudwatch put-metric-alarm --alarm-name disk-mon-2  --alarm-description "Alarm when Prod 2 storage exceeds 80%" --metric-name DiskSpaceUtilization --namespace System/Linux --statistic Maximum --period 60 --threshold 80 --comparison-operator GreaterThanThreshold --evaluation-periods 1 --alarm-actions $snsArn2 --unit Percent --dimensions Name=InstanceId,Value=$instanceId3 Name=MountPath,Value=/ Name=Filesystem,Value=/dev/xvda1
+
 echo "Done."
