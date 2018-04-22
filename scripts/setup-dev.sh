f451b4c665f9dbc68031d01718df2d7d4bee982e
josefmonje <josefmonje@gmail.com>
2018-04-22 18:34:18 +0800

    include key pair in process, improve keypair name variable, rm stricthostkeychecking

--- scripts/setup-dev.sh
+++ scripts/setup-dev.sh
@@ -2,34 +2,37 @@
 
 echo "Loading"
 source scripts/vars.sh
-# echo "Creating key"
-# aws ec2 create-key-pair --key-name $keyName --query 'KeyMaterial' --output text > $keyFile
-# chmod 400 $keyFile
+echo "Creating key"
+aws ec2 create-key-pair --key-name $keyPairName --query 'KeyMaterial' --output text > $keyFile
+chmod 400 $keyFile
 
 echo "Setting up Dev Server..."
-instanceId1=`aws ec2 run-instances --image-id $amiId --count 1 --instance-type t2.nano --key-name $keyName --security-group-ids $securityGroupId1 --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
+instanceId1=`aws ec2 run-instances --image-id $amiId --count 1 --instance-type t2.nano --key-name $keyPairName --security-group-ids $securityGroupId1 --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
 instance1Url=`aws ec2 describe-instances --instance-ids $instanceId1 --query 'Reservations[0].Instances[0].PublicDnsName' --output text`
 echo $instanceId1 > var/id.instance1
 echo $instance1Url > var/url.instance1
 echo "Dev Server ($instanceId1) at $instance1Url"
+sleep 35
 
 echo "Adding Me to authorized keys"
-cat $myPubKey | ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url 'cat >> ~/.ssh/authorized_keys'
+# cat $myPubKey | ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url 'cat >> ~/.ssh/authorized_keys'
+cat $myPubKey | ssh -i $keyFile ubuntu@$instance1Url 'cat >> ~/.ssh/authorized_keys'
+sleep 5
 
 echo "Generating Dev ssh key"
-ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url '
+ssh -i $keyFile ubuntu@$instance1Url '
   ssh-keygen -t rsa -q -f "/home/ubuntu/.ssh/id_rsa" -N ""; \
   cat ${HOME}/.ssh/id_rsa.pub; \
 ' > var/ssh-key.dev
 echo "Dev ssh key generated"
 
 echo "Retrieving Dev IP address"
-devIp=`ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url 'curl http://instance-data/latest/meta-data/public-ipv4'`
+devIp=`ssh -i $keyFile ubuntu@$instance1Url 'curl http://instance-data/latest/meta-data/public-ipv4'`
 echo $devIp > var/ip.instance1
 echo "IP address is $devIp"
 
 echo "Installing..."
-ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url '
+ssh -i $keyFile ubuntu@$instance1Url '
   sudo fallocate -l 2G /swapfile; \
   sudo chmod 600 /swapfile; \
   sudo mkswap /swapfile; \
@@ -44,14 +47,14 @@ ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url '
 echo "Done installing"
 
 echo "Generating Jenkins ssh key"
-ssh -i $keyFile -o StrictHostKeyChecking=no ubuntu@$instance1Url '
+ssh -i $keyFile ubuntu@$instance1Url '
   sudo -u jenkins ssh-keygen -t rsa -q -f "/var/lib/jenkins/.ssh/id_rsa" -N ""; \
   sudo cat /var/lib/jenkins/.ssh/id_rsa.pub; \
 ' > var/ssh-key.jenkins
 echo "Jenkins ssh key generated"
 
 echo "Sending AWS credentials"
-scp -i $keyFile -o StrictHostKeyChecking=no ~/.aws/* ubuntu@$instance1Url:~/.aws/.
+scp -i $keyFile ~/.aws/* ubuntu@$instance1Url:~/.aws/.
 echo "Assigning aws creds to Jenkins"
 ssh -i $keyFile ubuntu@$instance1Url '
   sudo chown jenkins:jenkins -R ~/.aws; \
@@ -64,7 +67,7 @@ aws ec2 authorize-security-group-ingress --group-id $securityGroupId1 --protocol
 aws ec2 authorize-security-group-ingress --group-id $securityGroupId2 --protocol tcp --port 22 --cidr $devIp/32
 
 echo "Sending variables..."
-scp -i $keyFile -o StrictHostKeyChecking=no var/* ubuntu@$instance1Url:var/.
+scp -i $keyFile var/* ubuntu@$instance1Url:var/.
 
 jenkinsPassword=`ssh -i $keyFile ubuntu@$instance1Url 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword'`
 echo $jenkinsPassword > var/password.jenkins
