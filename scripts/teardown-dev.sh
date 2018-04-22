829dd4c72a6939f1375e70472b5a01e1cd297add
josefmonje <josefmonje@gmail.com>
2018-04-22 18:29:49 +0800

    delete key file and key pair name in dev teardown

--- scripts/teardown-dev.sh
+++ scripts/teardown-dev.sh
@@ -6,4 +6,10 @@ source scripts/vars.sh
 echo "Terminating..."
 aws ec2 terminate-instances --instance-ids $instanceId1
 
+echo "Deleting key pair"
+aws ec2 delete-key-pair --key-name $keyPairName
+
+echo "Deleting key file"
+sudo rm $keyFile
+
 echo "Done!"
